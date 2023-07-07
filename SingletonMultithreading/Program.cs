5c661bb579cd3750c730277231c43f7b5dd81ec8
Piersilvio Spicoli <88087303+Piersilvio@users.noreply.github.com>
2023-07-07 11:20:26 +0200

    upload singleton class with multithreading approac

---
+++ SingletonMultithreading/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace SingletonMultithreading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // The client code.
+
+            Console.WriteLine(
+                "{0}\n{1}\n\n{2}\n",
+                "If you see the same value, then singleton was reused (yay!)",
+                "If you see different values, then 2 singletons were created (booo!!)",
+                "RESULT:"
+            );
+
+            Thread process1 = new Thread(() =>
+            {
+                TestSingleton("FOO");
+            });
+            Thread process2 = new Thread(() =>
+            {
+                TestSingleton("BAR");
+            });
+
+            process1.Start();
+            process2.Start();
+
+            process1.Join();
+            process2.Join();
+        }
+
+        public static void TestSingleton(string value)
+        {
+            Singleton singleton = Singleton.GetInstance(value);
+            Console.WriteLine(singleton.Value);
+        }
+    }
+
+    class Singleton
+    {
+        private Singleton() { }
+
+        private static Singleton? _instance;
+
+        private static readonly object _lock = new object();
+
+        public static Singleton GetInstance(string value)
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Singleton();
+                        _instance.Value = value;
+                    }
+                }
+            }
+            return _instance;
+        }
+
+        // We'll use this property to prove that our Singleton really works.
+        public string Value { get; set; }
+    }
+}
