1236e418c9d96f4b5b0c19939f6a61edc5818572
Piersilvio Spicoli <88087303+Piersilvio@users.noreply.github.com>
2023-07-07 11:19:44 +0200

    Upload the singleton without multithreading

---
+++ Singleton/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // The client code.
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+            }
+        }
+    }
+
+    public sealed class Singleton
+    {
+        private Singleton() { }
+
+        private static Singleton? _instance;
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+    }
+}
